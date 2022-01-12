02edbc13b11c2a9377a6fc17abb4bb1b8ae23cee
mosakoh <mokoh@twigeducation.com>
2022-01-12 15:24:29 +0000

    updated version and django version

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='django-url-mapper',
-    version='1.0.1',
+    version='1.0.2',
     author='Colin Barnwell',
     scripts=[],
     description='Use fixed keys in your Django template to refer to dynamic URLs',
     long_description=open('README.md').read(),
     install_requires=[
-        "Django >= 1.6",
+        "Django==1.11.29",
     ],
     packages=find_packages(),
     include_package_data=True
