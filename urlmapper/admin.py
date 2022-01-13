1d758ae300d857f7d6b239260c5b1444b050721d
mosakoh <mokoh@twigeducation.com>
2022-01-13 16:04:53 +0000

    fixed settings import

--- urlmapper/admin.py
+++ urlmapper/admin.py
@@ -3,18 +3,18 @@ from django.utils.translation import ugettext_lazy as _
 
 from .models import URLMap
 
-import settings
+from .settings import URLMAPPER_ALLOWED_MAPPINGS
 
 
 class URLMapAdmin(admin.ModelAdmin):
     list_display = ('key', 'mapping_type', 'get_url')
 
     fieldsets = [(None, {'fields': ('key',)})]
-    if 'url' in settings.URLMAPPER_ALLOWED_MAPPINGS:
+    if 'url' in URLMAPPER_ALLOWED_MAPPINGS:
         fieldsets.append((_("URL mapping"), {'fields': ('url',)}))
-    if 'object' in settings.URLMAPPER_ALLOWED_MAPPINGS:
+    if 'object' in URLMAPPER_ALLOWED_MAPPINGS:
         fieldsets.append((_("Object mapping"), {'fields': ('content_type', 'object_id')}))
-    if 'view_name' in settings.URLMAPPER_ALLOWED_MAPPINGS:
+    if 'view_name' in URLMAPPER_ALLOWED_MAPPINGS:
         fieldsets.append((_("View mapping"), {'fields': ('view_name', 'view_keywords')}))
 
 
