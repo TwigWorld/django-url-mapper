b7dc42b8d503d64a68e50556d0c7ab3af23bee8e
Colin Ross <cross@twig-world.com>
2022-08-25 12:12:08 +0100

    Updated version and package requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-url-mapper',
-    version='1.0.2',
+    version='2.0.0',
     author='Colin Barnwell',
     scripts=[],
     description='Use fixed keys in your Django template to refer to dynamic URLs',
     long_description=open('README.md').read(),
+    python_requires='>=3.7.0'
     install_requires=[
-        "Django>=1.11.29",
+        "Django>=1.11.29, <2.0",
     ],
     packages=find_packages(),
     include_package_data=True
