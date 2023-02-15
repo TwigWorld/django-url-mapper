81c08905f699d1519462ded05f40fe29a2d0b0ef
Michael Davis <mdavis@twigeducation.com>
2023-02-15 10:34:31 +0000

    Move the settings set up into conftest

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+import os
+
+from django.conf import settings
+
+# PYTHON3 use pathlib
+TEST_FOLDER = os.path.dirname(os.path.abspath(__file__))
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urlmapper.settings")
+
+
+def pytest_configure():
+    # import pdb;pdb.set_trace()
+    # from urlmapper import settings
+    settings.configure(
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+            }
+        },
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.staticfiles',
+            'django.contrib.sites',
+            'urlmapper',
+        ],
+        STATIC_URL='/',
+        MIDDLEWARE_CLASSES=(
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+        ),
+        ROOT_URLCONF='tests.urls',
+        SITE_ID=1,
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ],
+        URLMAPPER_KEYS=[
+            'test_1',
+            'test_2',
+            'test_3',
+            'test_4',
+            'test_5'
+        ],
+        URLMAPPER_FUNCTIONS={
+            'test_1': lambda: 'test_1_success',
+            'test_2': lambda request: 'test_2_success'
+        },
+    )
